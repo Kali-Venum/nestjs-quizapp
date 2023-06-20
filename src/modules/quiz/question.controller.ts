fb1ba30581ea1f6b539ae406622ca32d183d9d61
weaverswebsubham <subham.das@weavers-web.com>
2023-06-20 17:55:59 +0530

    question repository &  service are added.

--- src/modules/quiz/question.controller.ts
+++ src/modules/quiz/question.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from "@nestjs/common";
+import { Controller } from '@nestjs/common';
+
+// Servicves.
+import { QuestionService } from './question.service';
 
 @Controller('Question')
 export class QuestionController {
-
+  constructor(private questionService: QuestionService) {}
 }
--- src/modules/quiz/question.service.ts
+++ src/modules/quiz/question.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+// Entity.
+import { Question } from './entity/question.entity';
 
 @Injectable()
 export class QuestionService {
-
+  constructor(
+    @InjectRepository(Question)
+    private QuestionRepository: Repository<Question>,
+  ) {}
 }
