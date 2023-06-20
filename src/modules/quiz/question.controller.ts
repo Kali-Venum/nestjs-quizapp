218a51a9794f41b2cb45e95662cbc0ab4a16a0b7
weaverswebsubham <subham.das@weavers-web.com>
2023-06-20 18:12:48 +0530

    api created to create a new question.

--- src/modules/quiz/question.controller.ts
+++ src/modules/quiz/question.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 // Servicves.
 import { QuestionService } from './question.service';
 
+// DTO.
+import { CreateQuestionDTO } from './dto/question.dto';
+
 @Controller('Question')
 export class QuestionController {
   constructor(private questionService: QuestionService) {}
+
+  @Post('/')
+  async createAQuestion(@Body() body: CreateQuestionDTO) {
+    return await this.questionService.createAQuestion(body);
+  }
 }
--- src/modules/quiz/question.service.ts
+++ src/modules/quiz/question.service.ts
@@ -5,10 +5,17 @@ import { Repository } from 'typeorm';
 // Entity.
 import { Question } from './entity/question.entity';
 
+// DTO.
+import { CreateQuestionDTO } from './dto/question.dto';
+
 @Injectable()
 export class QuestionService {
   constructor(
     @InjectRepository(Question)
     private QuestionRepository: Repository<Question>,
   ) {}
+
+  async createAQuestion(body: CreateQuestionDTO) {
+    return await this.QuestionRepository.save(body);
+  }
 }
