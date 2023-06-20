613aa80d13c34b4f571bceb3f28b19122e97a6db
weaverswebsubham <subham.das@weavers-web.com>
2023-06-20 17:50:30 +0530

    question service & question controller created.

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -3,18 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Controllers.
 import { QuizController } from './quiz.controller';
+import { QuestionController } from './question.controller';
 
 // Services.
 import { QuizService } from './quiz.service';
+import { QuestionService } from './question.service';
 
 // Entity.
 import { Quiz } from './entity/quiz.entity';
+import { Question } from './entity/question.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Quiz]),
-  ],
-  controllers: [QuizController],
-  providers: [QuizService],
+  imports: [TypeOrmModule.forFeature([Quiz, Question])],
+  controllers: [QuizController, QuestionController],
+  providers: [QuizService, QuestionService],
 })
 export class QuizModule {}
